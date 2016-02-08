b10330a99b6b6503743fc2ea80f3ed0c1a87aa8c
Cian Leow <cianyleow@gmail.com>
2016-02-08 15:44:51 +0000

    Added message to due-date directive.

--- app/modules/base/directives/dueDate.js
+++ app/modules/base/directives/dueDate.js
@@ -7,19 +7,24 @@ define([], function() {
 			scope: {
 				dateString: '=',
 			},
-			controller: ['$log', '$scope', '$filter', function($log, $scope, $filter) {
-				$scope.date = $filter('date')($scope.dateString, 'hh:mm:ss a dd/MMM/yyyy');
+			controller: ['$scope', '$filter', function($scope, $filter) {
 				var today = Date.now();
-				$log.debug(today);
 				var due = Date.parse($scope.dateString);
-				$log.debug(due);
-				if(today > due) {
+				var diff = due - today;
+				var daysLeft = Math.floor(diff / (3600 * 24 * 1000));
+				if(daysLeft < 0) {
 					$scope.late = true;
-				} else if(today - 3600*24*5 > due) {
+					$scope.message = "Late";
+				} else if(daysLeft < 1) {
 					$scope.close = true;
+					var hours = Math.floor(diff / (3600 * 1000));
+					var minutes = Math.ceil(Math.floor(diff % (3600 * 1000)) / (60 * 1000));
+					$scope.message = hours + " hours " + minutes + " minutes left";
 				} else {
 					$scope.early = true;
-				}
+					$scope.message = daysLeft + " days left";
+				} 
+				$scope.date = $filter('date')($scope.dateString, 'hh:mm:ss a dd/MMM/yyyy');
 			}],
 			link: function(scope, el, attrs) {
 				
