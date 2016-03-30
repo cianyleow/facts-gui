1cc3d5da01134df264d9b17fa149963cb63331e9
Cian Leow <cianyleow@gmail.com>
2016-03-30 18:22:57 +0100

    Made the list of all courses identify already enrolled courses.

--- app/modules/facts/student/course/controllers/coursesController.js
+++ app/modules/facts/student/course/controllers/coursesController.js
@@ -2,25 +2,20 @@ define([], function() {
 	'use strict';
 	return['$scope', 'Restangular', function($scope, Restangular) {
 		$scope.enrolledCourses = [];
-		$scope.allCourses = [];
-		
-		Restangular.one('enrollments').one('me').get().then(function(responses) {
+		Restangular.one('self').getList('courses').then(function(courses) {
+			$scope.enrolledCourses = courses;
 			var courseIds = [];
-			angular.forEach(responses, function(response) {
-				courseIds.push(response.courseId);
-				Restangular.one('courses', response.courseId).one('details').get().then(function(resp) {
-					$scope.enrolledCourses.push(resp);
-				});
+			angular.forEach(courses, function(course) {
+				courseIds.push(course.courseId);
 			});
-			
-			// Once enrollments have been received, get all courses and set enrollment level for those that are enrolled in.
-			Restangular.one('courses').get().then(function(responses) {
-				angular.forEach(responses, function(response) {
-					if(courseIds.indexOf(response.courseId) === -1) {
-						$scope.allCourses.push(response);
+			Restangular.all('courses').getList().then(function(courses) {
+				angular.forEach(courses, function(course) {
+					if(courseIds.indexOf(course.courseId) > -1) {
+						course.enrolled = true;
 					}
 				});
-			});
-		});
+				$scope.allCourses = courses;
+			});	
+		});		
 	}];
 });
