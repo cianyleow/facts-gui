ed9875146af7421130c5eabb1d5d0ab4a2a1c74b
Cian Leow <cianyleow@gmail.com>
2016-03-30 17:56:48 +0100

    Removed response interceptor to fix getList

--- app/app.js
+++ app/app.js
@@ -10,31 +10,6 @@ define(['angular', 'angular-material', 'angular-ui-breadcrumbs', 'angular-ui-tit
 			
 			RestangularProvider.setBaseUrl('http://localhost:8080');
 			
-			RestangularProvider.addResponseInterceptor(function(data, operation, route) {
-                var returnData;
-                if (operation === 'getList' && data._embedded) {
-                    returnData = data._embedded[route];
-                    /*
-                     Fallback: if the returneddata does not contain the route key take the first one.
-                     Example: data = { _embedded: { travelExpenses: [] } } but route = 'expenses'.
-                     */
-                    if (!returnData) {
-                        returnData = data._embedded[Object.keys(data._embedded)[0]];
-                    }
-                    //if there is pagination info make it one-based.
-                    if (data.page) {
-                        returnData.page = data.page;
-                        returnData.page.number = returnData.page.number + 1;
-                    }
-                } else if (operation === 'getList' && !data._embedded) {
-                    returnData = [];
-                    returnData.page = data.page;
-                } else {
-                    returnData = data;
-                }
-                return returnData;
-            });
-			
 			// We can not inject $state directly as this gives a circular dependency
             $httpProvider.interceptors.push(['$q', '$injector', function($q, $injector) {
                 return {
