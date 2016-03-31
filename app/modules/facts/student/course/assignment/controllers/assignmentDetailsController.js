d9943bb36db41627e5809e333b27028d6f208523
Cian Leow <cianyleow@gmail.com>
2016-03-31 18:12:58 +0100

    Made anonymous download work.

--- app/modules/facts/student/course/assignment/controllers/assignmentDetailsController.js
+++ app/modules/facts/student/course/assignment/controllers/assignmentDetailsController.js
@@ -1,13 +1,20 @@
 define([], function() {
 	'use strict';
-	return['$scope', '$stateParams', 'Restangular', 'base.services.piechart', function($scope, $stateParams, Restangular, PieChartService) {
+	return['$scope', '$stateParams', 'Restangular', 'base.services.piechart', '$window', function($scope, $stateParams, Restangular, PieChartService, $window) {
 		var assignment = Restangular.one('assignments', $stateParams.assignmentId);
 		$scope.assignment = assignment.get().$object;
 		$scope.requiredFiles = assignment.getList('requiredFiles').$object;
+		$scope.suppliedFiles = assignment.getList('suppliedFiles').$object;
 		$scope.markComponentOptions =  {thickness: 50};
 		$scope.markComponentData = [];
 		$scope.markComponents = assignment.getList('markComponents').then(function(data) {
 			$scope.markComponentData = PieChartService.getPieChartLines(data);
 		});
+		
+		$scope.download = function(fileId) {
+			Restangular.one('files', fileId).one('link').get().then(function(fileLink) {
+				$window.open('http://localhost:8080/files/' + fileLink.link + '/download');
+			});
+		}
 	}];
 });
