8246a17a34b136559c23d8d30bd03855a5f07969
Cian Leow <cianyleow@gmail.com>
2016-04-11 19:31:32 +0100

    Displaying new submissions now.

--- app/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
+++ app/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
@@ -1,7 +1,8 @@
 define([], function() {
 	'use strict';
-	return['$scope', '$state', '$stateParams', 'facts.services.submission', function($scope, $state, $stateParams, SubmissionService) {
-		$scope.submission = SubmissionService.getSubmission($stateParams.submissionId);
-		$scope.submissionVersion = 1;
+	return['$scope', '$state', '$stateParams', 'Restangular', function($scope, $state, $stateParams, Restangular) {
+		var submission = Restangular.one('submissions', $stateParams.submissionId);
+		$scope.submission = submission.get().$object;
+		$scope.submissionFiles = submission.getList('submissionFiles').$object;
 	}];
 });
