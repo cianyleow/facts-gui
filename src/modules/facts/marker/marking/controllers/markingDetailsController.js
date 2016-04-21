42600eb8b90b331d89b95cf6c85996e22401be4d
Cian Leow <cianyleow@gmail.com>
2016-04-21 12:47:34 +0100

    Adding more action menus to submission page.

--- src/modules/facts/marker/marking/controllers/markingDetailsController.js
+++ src/modules/facts/marker/marking/controllers/markingDetailsController.js
@@ -11,5 +11,23 @@ define([], function() {
 		});
 		$scope.comments = feedback.getList('comments').$object;
 		$scope.marks = feedback.getList('marks').$object;
+
+		$scope.submissionActions = [
+			{
+				description: 'Download files as ZIP',
+				icon: 'folder',
+				action: function() {
+					console.log('Download as ZIP');
+				}
+			}
+		];
+
+		$scope.markActions = [
+
+		];
+
+		$scope.commentActions = [
+
+		];
 	}];
 });
