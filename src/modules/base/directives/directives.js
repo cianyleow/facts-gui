b85a172e35c3c8e3f297c3489a6dbc56d1563e57
Cian Leow <cianyleow@gmail.com>
2016-04-21 12:21:41 +0100

    Added directive for downloading files

--- src/modules/base/directives/directives.js
+++ src/modules/base/directives/directives.js
@@ -1,9 +1,10 @@
-define(['./dueDate', './file'], function(dueDate, file) {
+define(['./dueDate', './file', './downloadButton'], function(dueDate, file, downloadButton) {
 	'use strict';
 	return {
 		init: function(module) {
 			module.directive('dueDate', dueDate);
 			module.directive('file', file);
+			module.directive('downloadButton', downloadButton);
 		}
 	};
 });
---
+++ src/modules/base/directives/downloadButton.js
@@ -0,0 +1,20 @@
+define([], function() {
+    'use strict';
+    return [function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'src/modules/base/directives/download-button.tpl.html',
+            scope: {
+                fileId: '=',
+            },
+            controller: ['$scope', 'Restangular', '$window', function($scope, Restangular, $window) {
+                $scope.download = function() {
+                    console.log($scope.fileId);
+                    Restangular.one('files', $scope.fileId).one('link').get().then(function(fileLink) {
+                        $window.open('api/files/' + fileLink.link + '/download');
+                    });
+                }
+            }]
+        };
+    }];
+});
