761f44eaf69dad9450e422b3a5d17a074e73cee2
Cian Leow <cianyleow@gmail.com>
2016-04-23 19:08:41 +0100

    Added dialog to change role

--- src/modules/base/controllers/toolbarController.js
+++ src/modules/base/controllers/toolbarController.js
@@ -1,12 +1,60 @@
 define([], function() {
 	'use strict';
-	return ['$scope', 'base.services.authentication', '$log', '$mdSidenav', 'base.services.token', function($scope, AuthenticationService, $log, $mdSidenav) {
+	return ['$scope', 'base.services.authentication', '$log', '$mdSidenav', 'base.services.token', '$mdDialog', function($scope, AuthenticationService, $log, $mdSidenav, TokenService, $mdDialog) {
+		var userInfo = TokenService.userInfo();
+
+		$scope.userDetails = userInfo.userDetails.firstName + ' ' + userInfo.userDetails.lastName + ' (' + userInfo.userDetails.userName + ')';
+
+		$scope.actions = [
+			{
+				icon: 'person',
+				description: 'Change Role',
+				action: function(targetEvent) {
+					$mdDialog.show(
+						{
+							controller: function($scope, $mdDialog) {
+								$scope.roles = userInfo.roles;
+								$scope.currentRole = userInfo.currentRole;
+								$scope.cancel = function() {
+									$mdDialog.cancel();
+								};
+
+								$scope.change = function(newRole) {
+									$mdDialog.hide(newRole);
+								};
+							},
+							templateUrl: 'src/modules/base/partials/role-change.dialog.tpl.html',
+							parent: angular.element(document.body),
+							targetEvent: targetEvent,
+							clickOutsideToClose: true
+						}
+					).then(function(newRole) {
+						console.log(newRole);
+						userInfo.currentRole = newRole;
+					}, function() {
+
+					});
+				}
+			},
+			{
+				icon: 'settings',
+				description: 'Settings',
+				action: function() {
+					alert('Settings!');
+				}
+			},
+			{
+				icon: 'power_settings_new',
+				description: 'Logout',
+				action: function() {
+					$log.info('Logging out user');
+					AuthenticationService.logout();
+				}
+			}
+		];
+
 		$scope.toggleSidenav = function(menuId) {
 			$mdSidenav(menuId).toggle();
-		};
-		$scope.logout = function() {
-			$log.debug('Logging out user');
-			AuthenticationService.logout();
 		};
 	}];
 });
