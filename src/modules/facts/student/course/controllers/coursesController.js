81af0d657e5d3f9f95a0ef889d230e85ee6fc844
Cian Leow <cianyleow@gmail.com>
2016-04-23 23:44:15 +0100

    Added inline enrollment option

--- src/modules/facts/student/course/controllers/coursesController.js
+++ src/modules/facts/student/course/controllers/coursesController.js
@@ -1,8 +1,8 @@
 define([], function() {
 	'use strict';
-	return['$scope', 'Restangular', function($scope, Restangular) {
+	return['$scope', 'Restangular', '$mdToast', '$mdDialog', function($scope, Restangular, $mdToast, $mdDialog) {
 		$scope.enrolledCourses = [];
-		Restangular.one('self').getList('courses').then(function(courses) {
+		Restangular.one('self').getList('enrollments').then(function(courses) {
 			$scope.enrolledCourses = courses;
 			var courseIds = [];
 			angular.forEach(courses, function(course) {
@@ -17,5 +17,24 @@ define([], function() {
 				$scope.allCourses = courses;
 			});
 		});
+
+		$scope.enroll = function(course, idx, event) {
+			var confirm = $mdDialog.confirm()
+				.title('Enroll in ' + course.shortName)
+				.textContent('Are you sure you want to enroll in ' + course.shortName + ": " + course.name + '?')
+				.ariaLabel('Enroll in course')
+				.targetEvent(event)
+				.ok('Enroll')
+				.cancel('Cancel');
+			$mdDialog.show(confirm).then(function() {
+				Restangular.one('courses', course.courseId).all('enrollments').post().then(function(_enrollment) {
+					$scope.enrolledCourses.push(course);
+					$scope.allCourses[idx].enrolled = true;
+					$mdToast.show($mdToast.simple().textContent('Enrolled in ' + course.shortName + ": " + course.name).position('top right'));
+				}, function(error) {
+					$mdToast.show($mdToast.simple().textContent('Failed to enroll in ' + course.shortName + ": " + course.name).position('top right'));
+				});
+			});
+		};
 	}];
 });
