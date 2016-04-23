c3f0732969d3ea732cd7ff4e79970cbf9d9158ba
Cian Leow <cianyleow@gmail.com>
2016-04-23 17:10:08 +0100

    Moved logout to authentication service

--- src/modules/base/controllers/toolbarController.js
+++ src/modules/base/controllers/toolbarController.js
@@ -1,13 +1,12 @@
 define([], function() {
 	'use strict';
-	return ['$scope', '$state', '$log', '$mdSidenav', 'base.services.token', function($scope, $state, $log, $mdSidenav, TokenService) {
+	return ['$scope', 'base.services.authentication', '$log', '$mdSidenav', 'base.services.token', function($scope, AuthenticationService, $log, $mdSidenav) {
 		$scope.toggleSidenav = function(menuId) {
 			$mdSidenav(menuId).toggle();
 		};
 		$scope.logout = function() {
 			$log.debug('Logging out user');
-			TokenService.clear();
-			$state.go('authorize');
+			AuthenticationService.logout();
 		};
 	}];
 });
