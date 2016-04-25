0ed8439dd9c45ae835c36acd3036dd8322b345c1
Cian Leow <cianyleow@gmail.com>
2016-04-25 16:53:32 +0100

    Simplified Courses/enrollment page

--- src/modules/facts/student/course/controllers/coursesController.js
+++ src/modules/facts/student/course/controllers/coursesController.js
@@ -1,78 +1,22 @@
 define([], function() {
 	'use strict';
 	return['$scope', 'Restangular', '$mdToast', '$mdDialog', function($scope, Restangular, $mdToast, $mdDialog) {
-		$scope.enrolledCourses = [];
-		Restangular.one('self').getList('enrollments').then(function(courses) {
-			$scope.enrolledCourses = courses;
+		$scope.enrollments = [];
+		Restangular.one('self').getList('enrollments').then(function(enrollments) {
+			$scope.enrollments = enrollments;
 			var courseIds = [];
-			angular.forEach(courses, function(course) {
-				courseIds.push(course.courseId);
+			angular.forEach(enrollments, function(enrollment) {
+				courseIds.push(enrollment.course.courseId);
 			});
 			Restangular.all('courses').getList().then(function(courses) {
+				$scope.allCourses = [];
 				angular.forEach(courses, function(course) {
-					if(courseIds.indexOf(course.courseId) > -1) {
-						course.enrolled = true;
+					if(courseIds.indexOf(course.courseId) === -1) {
+						$scope.allCourses.push(course);
 					}
 				});
-				$scope.allCourses = courses;
 			});
 		});
-
-		$scope.settings = function(course, idx, event) {
-			$mdDialog.show({
-				controller: function($scope) {
-					$scope.course = course;
-
-					Restangular.one('self').one('enrollments', course.courseId).get().then(function(_enrollment) {
-						$scope.enrollment = _enrollment;
-						$scope.enrollmentLevel = {level: _enrollment.enrollmentLevel};
-					});
-
-					$scope.enrollmentLevels = [
-						{
-							id: 0,
-							level: 'NO_CREDIT',
-							description: 'Not for credit'
-						},
-						{
-							id: 1,
-							level: 'SUBMISSION_CREDIT',
-							description: 'Credit for Submissions'
-						},
-						{
-							id:2,
-							level: 'EXAM_CREDIT',
-							description: 'Credit for Exams'
-						}
-					];
-
-					$scope.save = function(enrollment, enrollmentLevel) {
-						enrollment.enrollmentLevel = enrollmentLevel.level;
-						Restangular.one('enrollments', enrollment.enrollmentId).customPUT(enrollment).then(function() {
-
-						}, function() {
-
-						});
-					};
-
-					$scope.unenroll = function(enrollment) {
-						Restangular.one('enrollments', enrollment.enrollmentId).remove().then(function() {
-
-						}, function() {
-
-						});
-					};
-
-					$scope.cancel = function() {
-						$mdDialog.cancel();
-					};
-				},
-				templateUrl: 'src/modules/facts/student/course/partials/settings.tpl.html',
-				parent: angular.element(document.body),
-				targetEvent: event,
-				clickOutsideToClose: true
-			});
-		};
 
 		$scope.enroll = function(course, idx, event) {
 			var confirm = $mdDialog.confirm()
@@ -83,11 +27,15 @@ define([], function() {
 				.ok('Enroll')
 				.cancel('Cancel');
 			$mdDialog.show(confirm).then(function() {
-				Restangular.one('courses', course.courseId).all('enrollments').post().then(function() {
-					$scope.enrolledCourses.push(course);
-					$scope.allCourses[idx].enrolled = true;
+				course.enrolling = true;
+				course.failed = false;
+				Restangular.one('courses', course.courseId).all('enrollments').post().then(function(_enrollment) {
+					$scope.allCourses.splice(idx, 1);
+					$scope.enrollments.push(_enrollment);
 					$mdToast.show($mdToast.simple().textContent('Enrolled in ' + course.shortName + ': ' + course.name).position('top right'));
 				}, function() {
+					course.enrolling = false;
+					course.failed = true;
 					$mdToast.show($mdToast.simple().textContent('Failed to enroll in ' + course.shortName + ': ' + course.name).position('top right'));
 				});
 			});
