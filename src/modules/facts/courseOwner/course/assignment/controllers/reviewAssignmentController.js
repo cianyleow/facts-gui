44389a13bf8a411ae8f241397f02f2469659482c
Cian Leow <cianyleow@gmail.com>
2016-05-04 15:58:22 +0100

    Made assignment submission have some sort of error handling

--- src/modules/facts/courseOwner/course/assignment/controllers/reviewAssignmentController.js
+++ src/modules/facts/courseOwner/course/assignment/controllers/reviewAssignmentController.js
@@ -9,6 +9,10 @@ define([], function() {
 		$scope.markComponentOptions = {thickness: 30};
 		
 		$scope.create = function(assignment) {
+			$scope.error = undefined;
+			$scope.uploading = true;
+			// Create Promise that can be cancelled.
+			$scope.canceler = $q.defer();
 			// Create assignment
 			var baseAssignment = angular.copy(assignment);
 			baseAssignment.groupBased = undefined;
@@ -19,9 +23,27 @@ define([], function() {
 				fd.append('files', suppliedFile.file);
 			});
 			
-			Restangular.one('courses', $stateParams.courseId).all('assignments').withHttpConfig({transformRequest: angular.identity}).customPOST(fd, undefined, undefined, {'Content-Type': undefined}).then(function(_assignment) {
-				console.log(_assignment);
+			Restangular.one('courses', $stateParams.courseId).all('assignments').withHttpConfig({timeout: $scope.canceler.promise, transformRequest: angular.identity}).customPOST(fd, undefined, undefined, {'Content-Type': undefined}).then(function(_assignment) {
+				$scope.uploading = false;
+				$scope._assignment = _assignment;
+				$scope.canceler.reject();
+			}, function(_error) {
+				$scope.uploading = false;
+				$scope.canceler.reject();
+				$scope.error = {message: errorCodes[_error.status]};
 			});
+		};
+
+		var errorCodes = {
+			413: 'Attachments are too large for upload. Max combined upload size is 10Mb.',
+			500: 'Unspecified server error, please contact administrator.'
+		};
+
+		$scope.cancel = function(canceler) {
+			$scope.uploading = false;
+			if(canceler) {
+				canceler.resolve('Cancelled request');
+			}
 		};
 	}];
 });
