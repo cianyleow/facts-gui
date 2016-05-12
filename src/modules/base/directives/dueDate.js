f7a8a7243433e282c628181f0549531e53ec6aae
Cian Leow <cianyleow@gmail.com>
2016-05-12 22:39:49 +0100

    Changed from late to assignment closed status on assignments

--- src/modules/base/directives/dueDate.js
+++ src/modules/base/directives/dueDate.js
@@ -13,16 +13,18 @@ define([], function() {
 				var diff = due - today;
 				var daysLeft = Math.floor(diff / (3600 * 24 * 1000));
 				if(daysLeft < 0) {
-					$scope.late = true;
-					$scope.message = 'Late';
-				} else if(daysLeft < 1) {
-					$scope.close = true;
-					var hours = Math.floor(diff / (3600 * 1000));
-					var minutes = Math.ceil(Math.floor(diff % (3600 * 1000)) / (60 * 1000));
-					$scope.message = hours + ' hours ' + minutes + ' minutes left';
+					$scope.open = false;
 				} else {
-					$scope.early = true;
-					$scope.message = daysLeft + ' days left';
+					$scope.open = true;
+					if(daysLeft < 1) {
+						$scope.close = true;
+						var hours = Math.floor(diff / (3600 * 1000));
+						var minutes = Math.ceil(Math.floor(diff % (3600 * 1000)) / (60 * 1000));
+						$scope.message = hours + ' hours ' + minutes + ' minutes left';
+					} else {
+						$scope.early = true;
+						$scope.message = daysLeft + ' days left';
+					}
 				}
 				$scope.date = $filter('date')($scope.dateString, 'hh:mm:ss a dd/MMM/yyyy');
 			}]
