fb85c501f094ff0faca8177f149ce49ce54be8a2
Cian Leow <cianyleow@gmail.com>
2016-05-13 14:40:01 +0100

    Added announcements and creation of them

--- src/modules/facts/courseOwner/course/controllers/courseDetailsController.js
+++ src/modules/facts/courseOwner/course/controllers/courseDetailsController.js
@@ -1,11 +1,34 @@
 define([], function() {
 	'use strict';
-	return['$scope', '$stateParams', 'Restangular', function($scope, $stateParams, Restangular) {
+	return['$scope', '$stateParams', 'Restangular', '$mdDialog', 'base.services.dialog', function($scope, $stateParams, Restangular, $mdDialog, DialogService) {
 		var course = Restangular.one('courses', $stateParams.courseId);
 		$scope.course = course.get().$object;
 		$scope.assignments = course.getList('assignments').$object;
 		$scope.courseOwners = course.getList('courseOwners').$object;
 		$scope.markers = course.getList('markers').$object;
 		$scope.students = course.getList('students').$object;
+		$scope.announcements = course.getList('announcements').$object;
+
+		$scope.newAnnouncement = function(targetEvent) {
+			DialogService.showCustomDialog(['$scope', '$mdDialog', function($scope, $mdDialog) {
+					$scope.announcement = {};
+					$scope.cancel = function() {
+						$mdDialog.cancel();
+					};
+
+					$scope.check = function(announcement) {
+						$mdDialog.hide(announcement);
+					};
+				}], 'src/modules/facts/courseOwner/course/partials/new.announcementDialog.tpl.html', angular.element(document.body), targetEvent,
+				function(announcement) {
+					Restangular.one('courses', $stateParams.courseId).all('announcements').post(announcement).then(function(_announcement) {
+						$scope.announcements.push(_announcement);
+					}, function(_error) {
+
+					});
+				}, function() {
+					// Closed
+				});
+		};
 	}];
 });
