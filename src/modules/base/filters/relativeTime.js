672bb73e8a55d83247ef71220fcb91ebda05a7ff
Cian Leow <cianyleow@gmail.com>
2016-05-15 22:14:48 +0100

    Relative time corrected for one hour

--- src/modules/base/filters/relativeTime.js
+++ src/modules/base/filters/relativeTime.js
@@ -5,16 +5,18 @@ define([], function() {
 		return function (date) {
 			var currentDate = new Date();
 			var timeDiff = currentDate - date;
-			var seconds = Math.round(timeDiff / 1000);
+			var seconds = Math.round((timeDiff / 1000) - 0.5);
 			if(seconds < 60) {
 				return 'just now';
 			} else if(seconds >= 60 && seconds < 120) {
 				return 'a minute ago';
 			} else if(seconds >= 120 && seconds < 3600) {
 				var currDate = new Date(date);
-				return Math.round(seconds / 60) + ' minutes ago';
-			} else if(seconds >= 3600 &&  seconds < 86400) {
-				return Math.round(seconds / 3600) + ' hours ago';
+				return Math.round((seconds / 60) - 0.5) + ' minutes ago';
+			} else if(seconds >= 3600 &&  seconds < 7200) {
+				return 'an hour ago';
+			} else if(seconds >= 7200 && seconds < 86400) {
+				return Math.round((seconds / 3600) - 0.5) + ' hours ago';
 			} else if(seconds >= 86400) {
 				return 'on ' + $filter('date')(currDate, 'dd MMM, yyyy');
 			}
