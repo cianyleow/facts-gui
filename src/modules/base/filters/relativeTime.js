751025e5998c637febdbc02caa8ff7685f3a5087
Cian Leow <cianyleow@gmail.com>
2016-05-18 00:38:12 +0100

    Relative time does negative time too!

--- src/modules/base/filters/relativeTime.js
+++ src/modules/base/filters/relativeTime.js
@@ -3,22 +3,28 @@ define([], function() {
 
 	var relativeTimeFilter = ['$filter', function($filter) {
 		return function (date) {
+			var targetDate = new Date(date);
 			var currentDate = new Date();
 			var timeDiff = currentDate - date;
 			var seconds = Math.round((timeDiff / 1000) - 0.5);
-			if(seconds < 60) {
+			if(seconds < -86400) {
+				return 'on ' + $filter('date')(targetDate, 'dd MMM, yyyy');
+			} else if(seconds <= -7200 && seconds > -86400) {
+				return 'in ' + Math.round((-seconds / 3600) -0.5) + ' hours';
+			} else if(seconds <= 0 && seconds > -7200) {
+				return 'at ' + $filter('date')(targetDate, 'HH:mm');
+			} else if(seconds >= 0 && seconds < 60) {
 				return 'just now';
 			} else if(seconds >= 60 && seconds < 120) {
 				return 'a minute ago';
 			} else if(seconds >= 120 && seconds < 3600) {
-				var currDate = new Date(date);
 				return Math.round((seconds / 60) - 0.5) + ' minutes ago';
 			} else if(seconds >= 3600 &&  seconds < 7200) {
 				return 'an hour ago';
 			} else if(seconds >= 7200 && seconds < 86400) {
 				return Math.round((seconds / 3600) - 0.5) + ' hours ago';
 			} else if(seconds >= 86400) {
-				return 'on ' + $filter('date')(currDate, 'dd MMM, yyyy');
+				return 'on ' + $filter('date')(targetDate, 'dd MMM, yyyy');
 			}
 		};
 	}];
