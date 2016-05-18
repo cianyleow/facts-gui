3754fd35412fab20f9d24d9fc05e868a4d60e86d
Cian Leow <cianyleow@gmail.com>
2016-05-18 15:59:27 +0100

    Added in progress directive

--- src/modules/base/directives/directives.js
+++ src/modules/base/directives/directives.js
@@ -1,4 +1,4 @@
-define(['./dueDate', './file', './downloadButton', './searchFilter', './autoFocus', './shakeOn', './countdownAction'], function(dueDate, file, downloadButton, searchFilter, autoFocus, shakeOn, countdownAction) {
+define(['./dueDate', './file', './downloadButton', './searchFilter', './autoFocus', './shakeOn', './countdownAction', './inProgress'], function(dueDate, file, downloadButton, searchFilter, autoFocus, shakeOn, countdownAction, inProgress) {
 	'use strict';
 	return {
 		init: function(module) {
@@ -9,6 +9,7 @@ define(['./dueDate', './file', './downloadButton', './searchFilter', './autoFocu
 			module.directive('autoFocus', autoFocus);
 			module.directive('shakeOn', shakeOn);
 			module.directive('countdownAction', countdownAction);
+			module.directive('inProgress', inProgress);
 		}
 	};
 });
---
+++ src/modules/base/directives/inProgress.js
@@ -0,0 +1,18 @@
+define([], function() {
+    'use strict';
+    return ['$mdDialog', function($mdDialog) {
+        return {
+            restrict: 'A',
+            scope: {
+                inProgress: '='
+            },
+            link: function(scope, element) {
+                element.bind('click', function() {
+                    if(scope.inProgress == true) {
+                        $mdDialog.show($mdDialog.alert({title: 'In Progress', textContent: 'This functionality is still a work in progress. Please check back later for a working version.', ok: 'Close'}));
+                    }
+                });
+            }
+        };
+    }];
+});
