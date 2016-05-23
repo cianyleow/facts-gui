1445ef6c8e175ea7cb878b0049a5404786415538
Cian Leow <cianyleow@gmail.com>
2016-05-23 23:57:00 +0100

    Changed to complex assignment objects

--- src/modules/facts/student/course/assignment/controllers/assignmentDetailsController.js
+++ src/modules/facts/student/course/assignment/controllers/assignmentDetailsController.js
@@ -3,8 +3,6 @@ define([], function() {
 	return['$scope', '$stateParams', 'Restangular', '$window', 'facts.services.assignment', function($scope, $stateParams, Restangular, $window, AssignmentService) {
 		var assignment = Restangular.one('assignments', $stateParams.assignmentId);
 		$scope.assignment = AssignmentService.getAssignment($stateParams.assignmentId);
-		$scope.requiredFiles = assignment.getList('requiredFiles').$object;
-		$scope.suppliedFiles = assignment.getList('suppliedFiles').$object;
 		$scope.submissions = assignment.one('submissions').getList('self').$object;
 
 		$scope.submissionIcons = {
