fdbe76d53b6301aab9f78430203f6f290a7a0c16
Cian Leow <cianyleow@gmail.com>
2016-05-24 11:48:34 +0100

    Refactored page to use more complex API objects

--- src/modules/facts/courseOwner/course/controllers/courseDetailsController.js
+++ src/modules/facts/courseOwner/course/controllers/courseDetailsController.js
@@ -3,13 +3,10 @@ define([], function() {
 	return['$scope', '$stateParams', 'Restangular', '$mdDialog', 'base.services.dialog', '$mdToast', function($scope, $stateParams, Restangular, $mdDialog, DialogService, $mdToast) {
 		$scope.editCourse = false;
 
-		var course = Restangular.one('courses', $stateParams.courseId);
+		var course = Restangular.one('courses', $stateParams.courseId).one('admin');
 		$scope.course = course.get().$object;
-		$scope.assignments = course.getList('assignments').$object;
-		$scope.courseOwners = course.getList('courseOwners').$object;
 		$scope.markers = course.getList('markers').$object;
 		$scope.students = course.getList('students').$object;
-		$scope.announcements = course.getList('announcements').$object;
 
 		$scope.edit = function(course) {
 			$scope.editCourse = true;
@@ -47,7 +44,7 @@ define([], function() {
 				}], 'src/modules/facts/courseOwner/course/partials/new.announcementDialog.tpl.html', angular.element(document.body), targetEvent,
 				function(announcement) {
 					Restangular.one('courses', $stateParams.courseId).all('announcements').post(announcement).then(function(_announcement) {
-						$scope.announcements.push(_announcement);
+						$scope.course.announcements.push(_announcement);
 					}, function(_error) {
 						$mdToast.show($mdToast.simple().textContent('Failed to create announcement.').position('top right'));
 					});
@@ -65,8 +62,8 @@ define([], function() {
 			$mdDialog.show(confirm).then(function() {
 				announcement.deleting = true;
 				announcement.failed = false;
-				Restangular.one('courses', $stateParams.courseId).one('announcements', announcement.announcementId).remove().then(function() {
-					$scope.announcements.splice($scope.announcements.indexOf(announcement), 1);
+				Restangular.one('announcements', announcement.announcementId).remove().then(function() {
+					$scope.course.announcements.splice($scope.course.announcements.indexOf(announcement), 1);
 					$mdToast.show($mdToast.simple().textContent('Deleted "' + announcement.title + '" announcement.').position('top right'));
 				}, function() {
 					announcement.deleting = false;
