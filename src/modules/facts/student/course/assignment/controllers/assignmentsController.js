07fab31e460739c2b585dc9848357e98a6e0d106
Cian Leow <cianyleow@gmail.com>
2016-05-24 11:39:11 +0100

    Fixed request for assignments page

--- src/modules/facts/student/course/assignment/controllers/assignmentsController.js
+++ src/modules/facts/student/course/assignment/controllers/assignmentsController.js
@@ -1,6 +1,6 @@
 define([], function() {
 	'use strict';
-	return['$scope', '$stateParams', 'Restangular', function($scope, $stateParams, Restangular) {
-		$scope.assignments = Restangular.one('courses', $stateParams.courseId).getList('assignments').$object;
+	return['$scope', '$stateParams', 'facts.services.course', function($scope, $stateParams, CourseService) {
+		$scope.assignments = CourseService.getCourse($stateParams.courseId).assignments;
 	}];
 });
