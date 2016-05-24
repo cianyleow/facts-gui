becfa0426438aa03d25f8eb4a184c0f05d099454
Cian Leow <cianyleow@gmail.com>
2016-05-24 01:17:22 +0100

    Displaying simpler feedback/submission objects.

--- src/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
+++ src/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
@@ -4,16 +4,5 @@ define([], function() {
 		$scope.assignment = AssignmentService.getAssignment($stateParams.assignmentId);
 		var submission = Restangular.one('submissions', $stateParams.submissionId);
 		$scope.submission = submission.get().$object;
-		$scope.submissionFiles = submission.getList('submissionFiles').$object;
-		submission.one('feedback').get().then(function(_feedback) {
-			if(_feedback.commentReleased) {
-				_feedback.comments = submission.one('feedback', _feedback.feedbackId).getList('comments').$object;
-			}
-
-			if(_feedback.markReleased) {
-				_feedback.grade = submission.one('feedback', _feedback.feedbackId).one('grade').get().$object;
-			}
-			$scope.feedback = _feedback;
-		});
 	}];
 });
