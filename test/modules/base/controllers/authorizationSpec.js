e6cb39608cd0da6f4f9dfb6e3d239d17431d8097
Cian Leow <cianyleow@gmail.com>
2016-05-30 13:46:38 +0100

    Added test for authorization controller

---
+++ test/modules/base/controllers/authorizationSpec.js
@@ -0,0 +1,50 @@
+define(['baseTestSetup'], function(baseTestSetup) {
+    'use strict';
+    describe('base.controllers.authorization', function() {
+        var AuthorizationController, scope, AuthenticationService;
+        baseTestSetup();
+
+        beforeEach(inject(function($rootScope, $controller, $injector) {
+            scope = $rootScope.$new();
+            AuthenticationService = $injector.get('base.services.authentication');
+            AuthorizationController = $controller('base.controllers.authorization-controller', {
+                $scope: scope
+            });
+        }));
+
+        it('Should define a user, error, attempts and states.', inject(function($httpBackend) {
+            expect(scope.user).toBeDefined();
+            expect(scope.timeout).toEqual(false);
+            expect(scope.error).toEqual(false);
+            expect(scope.attempts).toEqual(0);
+            expect(scope.states).toBeDefined();
+        }));
+
+        it('Authentication request should use AuthenticationService', function() {
+            spyOn(AuthenticationService, 'login').andCallFake(function() {})
+            var user = {};
+            scope.authenticate(user);
+            expect(AuthenticationService.login).toHaveBeenCalled();
+        });
+
+        it('Failed authentication should invoke only error mode and increase attempts', function() {
+            spyOn(AuthenticationService, 'login').andCallFake(function(user, redirect, callback) { callback({ status: 404})});
+            var user = {};
+            scope.authenticate(user);
+            scope.$digest();
+            expect(scope.error).toEqual(true);
+            expect(scope.timeout).toEqual(false);
+            expect(scope.attempts).toEqual(1);
+        });
+
+        it('Timed out authentication should invoke only timeout mode and increase attempts', function() {
+            spyOn(AuthenticationService, 'login').andCallFake(function(user, redirect, callback) { callback({ status: -1})});
+            var user = {};
+            scope.authenticate(user);
+            scope.$digest();
+            expect(scope.error).toEqual(false);
+            expect(scope.timeout).toEqual(true);
+            expect(scope.attempts).toEqual(1);
+        });
+    });
+});
