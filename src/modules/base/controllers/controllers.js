96eff40da2659e81931cb550c8c4e453668df5b7
Cian Leow <cianyleow@gmail.com>
2016-05-30 13:50:48 +0100

    Removed user menu controller

--- src/modules/base/controllers/controllers.js
+++ src/modules/base/controllers/controllers.js
@@ -1,17 +1,15 @@
 define(['modules/base/controllers/sidenavStudentController',
 	'modules/base/controllers/sidenavMarkerController',
 	'modules/base/controllers/sidenavCourseOwnerController',
-	'modules/base/controllers/userMenuController',
 	'modules/base/controllers/toolbarController',
 	'modules/base/controllers/authorizationController'
-], function(SidenavStudentController, SidenavMarkerController, SidenavCourseOwnerController, UserMenuController, ToolbarController, AuthorizationController) {
+], function(SidenavStudentController, SidenavMarkerController, SidenavCourseOwnerController, ToolbarController, AuthorizationController) {
 	'use strict';
 	return {
 		init: function(module) {
 			module.controller('base.controllers.sidenav.student.controller', SidenavStudentController);
 			module.controller('base.controllers.sidenav.marker.controller', SidenavMarkerController);
 			module.controller('base.controllers.sidenav.courseOwner.controller', SidenavCourseOwnerController);
-			module.controller('base.controllers.user-menu-controller', UserMenuController);
 			module.controller('base.controllers.toolbar-controller', ToolbarController);
 			module.controller('base.controllers.authorization-controller', AuthorizationController);
 		}
