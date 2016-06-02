82e8d26525211682554bc9cd323af6caac48aeea
Cian Leow <cianyleow@gmail.com>
2016-06-02 10:50:03 +0100

    Showing  comments/mark on page correctly now.

--- src/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
+++ src/modules/facts/student/course/assignment/submission/controllers/submissionDetailsController.js
@@ -3,6 +3,9 @@ define([], function() {
 	return['$scope', '$state', '$stateParams', 'Restangular', 'facts.services.assignment', function($scope, $state, $stateParams, Restangular, AssignmentService) {
 		$scope.assignment = AssignmentService.getAssignment($stateParams.assignmentId);
 		var submission = Restangular.one('submissions', $stateParams.submissionId);
-		$scope.submission = submission.get().$object;
+		submission.get().then(function(_submission) {
+			$scope.submission = _submission;
+			$scope.submission.feedback.comments = submission.one('feedback', _submission.feedback.feedbackId).getList('comments').$object;
+		});
 	}];
 });
