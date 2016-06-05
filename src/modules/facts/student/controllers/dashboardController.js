48ca4cd9630e162caa9e77ddb089d686cca66c43
Cian Leow <cianyleow@gmail.com>
2016-06-05 23:28:22 +0100

    Added announcements

--- src/modules/facts/student/controllers/dashboardController.js
+++ src/modules/facts/student/controllers/dashboardController.js
@@ -3,6 +3,15 @@ define([], function() {
 	return ['$scope', 'Restangular', function($scope, Restangular) {
 		$scope.notifications = Restangular.one('self').getList('notifications').$object;
 
+		$scope.announcementCourses = [];
+		Restangular.one('self').getList('enrolledCourses').then(function(enrolledCourses) {
+			angular.forEach(enrolledCourses, function(enrolledCourse) {
+				Restangular.one('courses', enrolledCourse.courseId).then(function(course) {
+					$scope.announcementCourses.push(course);
+				});
+			});
+		});
+
 		$scope.toggleFiltering = function() {
 			if($scope.notifications.length != 0) {
 				$scope.filtering = !$scope.filtering;
