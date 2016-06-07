efc24a6b0ab174a76299d990180004c6c8813e2c
Cian Leow <cianyleow@gmail.com>
2016-06-07 22:21:01 +0100

    Changed to min versions of dependencies

--- bootstrap.prod.js
+++ bootstrap.prod.js
@@ -1,13 +1,13 @@
 require.config({
 	paths: {
-		'angular': 'src/bower_components/angular/angular',
+		'angular': 'src/bower_components/angular/angular.min',
 		'angular-ui-router': 'src/bower_components/angular-ui-router/release/angular-ui-router.min',
 		'restangular': 'src/bower_components/restangular/dist/restangular.min',
 		'lodash': 'src/bower_components/lodash/lodash',
-		'angular-file-upload': 'src/bower_components/angular-file-upload/dist/angular-file-upload',
-		'angular-material': 'src/bower_components/angular-material/angular-material',
-		'angular-animate': 'src/bower_components/angular-animate/angular-animate',
-		'angular-aria': 'src/bower_components/angular-aria/angular-aria',
+		'angular-file-upload': 'src/bower_components/angular-file-upload/dist/angular-file-upload.min',
+		'angular-material': 'src/bower_components/angular-material/angular-material.min',
+		'angular-animate': 'src/bower_components/angular-animate/angular-animate.min',
+		'angular-aria': 'src/bower_components/angular-aria/angular-aria.min',
 		'angular-ui-breadcrumbs': 'src/bower_components/angular-utils-ui-breadcrumbs/uiBreadcrumbs',
 		'angular-ui-title': 'src/bower_components/angular-ui-router-title/angular-ui-router-title',
 		'ng-file-upload': 'src/bower_components/ng-file-upload/ng-file-upload.min',
